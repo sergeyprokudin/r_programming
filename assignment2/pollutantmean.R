c5ffb6a915e8fde8c48a2688a5a3c72c939998ae
unknown <prokudin@prokudin.avp.ru>
2014-10-21 19:34:33 +0400

    part1 completed

--- assignment2/pollutantmean.R
+++ assignment2/pollutantmean.R
@@ -1,4 +1,4 @@
-function(directory, pollutant, id = 1:332) {
+pollutantmean <- function(directory, pollutant, id = 1:332) {
         ## 'directory' is a character vector of length 1 indicating
         ## the location of the CSV files
         
